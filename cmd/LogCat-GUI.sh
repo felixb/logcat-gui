6fa000b1d7b5031339a7d14746cc6ce346163772
Felix Bechstein <f@ub0r.de>
2013-08-26 10:08:14 +0530

    search adb with locate

--- cmd/LogCat-GUI.sh
+++ cmd/LogCat-GUI.sh
@@ -2,4 +2,17 @@
 
 path="$(dirname $0)"
 
+if (which adb >/dev/null 2>/dev/null) then
+  echo found adb: $(which adb)
+else
+  adb=$(locate -b -r ^adb$ 2>/dev/null | grep platform-tools | head -n1)
+  if [ -n "${adb}" ] && [ -x "${adb}" ] ; then
+    echo found adb: ${adb}
+    PATH="$(dirname ${adb}):$PATH"
+    export PATH
+  else
+    echo "warning: adb not found"
+  fi
+fi
+
 java -jar "${path}/LogCat-GUI.jar"
